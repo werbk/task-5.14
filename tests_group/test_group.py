166e8fbe2616da1dc53f243bb41fb4ee6ddf456a
werbk <werbk@bk.ru>
2015-06-20 23:15:00 +0300

    add new test

---
+++ tests_group/test_group.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from group_lib import Group
+from fixture.variables import Profinity
+from random import randrange
+
+
+def test_create_group(app):
+    """Validation of correct create test group (All field fill up)"""
+
+    old_groups = app.group.get_group_list()
+    group = Group(group_name=Profinity.correct_data, group_header=Profinity.correct_data,
+                  group_footer=Profinity.correct_data)
+    app.group.create(group)
+    app.group.click_group_page()
+
+    assert len(old_groups)+1 == app.group.count(), 'Group does not created'
+
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    app.group.delete_first_group()
+    assert sorted(old_groups, key=Group.if_or_max) == sorted(new_groups, key=Group.if_or_max), 'Group list is different'
+
+
+def test_create_group_name(app):
+    """Validation of correct create test group (Only group name fill up)"""
+    old_groups = app.group.get_group_list()
+    group = Group(group_name='test')
+    app.group.create(group)
+
+    app.group.click_group_page()
+
+    assert len(old_groups)+1 == app.group.count(), 'Group does not created'
+    new_groups = app.group.get_group_list()
+    app.group.delete_first_group()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.if_or_max) == sorted(new_groups, key=Group.if_or_max), 'Group list is different'
+
+
+def test_edit_group(app):
+    """Validation of correct edit group (all field updated)"""
+
+    app.group.validation_of_group_exist()
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+
+    group = Group(group_name=Profinity.long_word_20, group_header=Profinity.long_word_20,
+                  group_footer=Profinity.long_word_20)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
+
+    assert len(old_groups) == app.group.count(), 'Group list changed'
+
+    new_groups = app.group.get_group_list()
+    app.group.delete_first_group()
+    old_groups[index] = group
+    assert sorted(old_groups, key=Group.if_or_max) == sorted(new_groups, key=Group.if_or_max), 'Group list is different'
+
+
+def test_delete_group(app):
+    """Validation of correct delete group"""
+
+    app.group.validation_of_group_exist()
+    old_groups = app.group.get_group_list()
+
+    index = randrange(len(old_groups))
+    app.group.delete_group_by_index(index)
+
+    assert len(old_groups)-1 == app.group.count(), 'Group does not created'
+    new_groups = app.group.get_group_list()
+    old_groups[index:index+1] = []
+    assert old_groups == new_groups, 'Group list is different'
